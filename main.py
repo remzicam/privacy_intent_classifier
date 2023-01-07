1fb19a3d1c8bba1db1236259aad9d32a277c7783
remzicam <65707131+remzicam@users.noreply.github.com>
2023-01-08 00:03:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import pipeline
 from pysbd import Segmenter
 
 app = FastAPI()
